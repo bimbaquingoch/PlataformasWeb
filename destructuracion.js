9ada54c1a423c14d76dfef545232fb60715f83a8
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-02-15 17:43:35 -0500

    arrow function en objeto

--- destructuracion.js
+++ destructuracion.js
@@ -9,14 +9,22 @@ const deadpool = {
   getName: function () {
     return `nombre: ${this.nombre} ${this.apellido}\npoder: ${this.poder}`;
   },
+  // con arrow function
+  // cuando se usa un arrow function se debe hacer referencia al objeto
+  getNombre: () => {
+    return `nombre: ${deadpool.nombre} ${deadpool.apellido}\npoder: ${deadpool.poder}`;
+  },
 };
 /*
 La destructuracion permite la extracción de variables o funciones de un objeto
   */
 // obtener las propiedades de un objeto por separado
 // opción 1
+console.log("******* getname function ******");
 console.log(deadpool.getName());
 
+console.log("\n****** getname arrow function *******");
+console.log(deadpool.getNombre());
 // para observar la informacion de un objeto de manera mas ordenada
 // utilizar console.table
 console.table(deadpool);
